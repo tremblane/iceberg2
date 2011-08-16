61d82a7eb4fb8a3bb57c38721d6a4bf326946d98
Jason Dew <jadew@cisco.com>
2011-08-16 18:09:20 -0400

    Move parsing and display to a subroutine Add looping

--- iceberg2.pl
+++ iceberg2.pl
@@ -17,122 +17,16 @@ print "\n";
 chomp($password);
 system("stty echo");
 
-get_page();
 
-my $simple = XML::Simple->new();
-my $tree = $simple->XMLin("$tempfile",ForceArray => 1);
-
-#get staffing count for talking agents
-foreach my $analyst (@{$tree->{agentstatus}->[0]->{talking}->[0]->{talkinganalyst}}) {
-	my @skills = split /,/, $analyst->{callskills};
-	foreach my $skill (@skills) {
-		if ($staffedskills{$skill}) {
-			$staffedskills{$skill} += 1;
-		} else {
-			$staffedskills{$skill} = 1;
-		}
-	}
-	if ($talkingskills{$analyst->{talkingon}}) {
-		$talkingskills{$analyst->{talkingon}} += 1;
-	} else {
-		$talkingskills{$analyst->{talkingon}} = 1;
-	}
+while ( "forever" ) {
+	get_page();
+	system('clear');
+	parse_and_display();
+	sleep(15);
 }
-
-#get staffing count for idle agents
-foreach my $analyst (@{$tree->{agentstatus}->[0]->{notready}->[0]->{notreadyanalyst}}) {
-	my @skills = split /,/, $analyst->{callskills};
-	foreach my $skill (@skills) {
-		if ($staffedskills{$skill}) {
-			$staffedskills{$skill} += 1;
-		} else {
-			$staffedskills{$skill} = 1;
-		}
-		if ($idleskills{$skill}) {
-			$idleskills{$skill} += 1;
-		} else {
-			$idleskills{$skill} = 1;
-		}
-	}
-}
-
-#get staffing count for ready agents
-foreach my $analyst (@{$tree->{agentstatus}->[0]->{ready}->[0]->{readyanalyst}}) {
-	my @skills = split /,/, $analyst->{callskills};
-	foreach my $skill (@skills) {
-		if ($staffedskills{$skill}) {
-			$staffedskills{$skill} += 1;
-		} else {
-			$staffedskills{$skill} = 1;
-		}
-		if ($readyskills{$skill}) {
-			$readyskills{$skill} += 1;
-		} else {
-			$readyskills{$skill} = 1;
-		}
-	}
-}
-
-#combine skills into groups
-foreach my $skill (sort keys %staffedskills) {
-	#set $group based on $skill
-	switch ($skill) {
-		case /GTRC_DESKTOP/ { $group=" DESKTOP"; }
-		case /GTRC_ENG/ { $group=" ENG"; }
-		case /GTRC_MAIN/ { $group=" MAIN"; }
-		case /GTRC_MOBILITY/ { $group=" MOBILITY"; }
-		case /GTRC_T2D_SPA/ { $group=" T2D_SPANISH"; }
-		case /GTRC_T2D/ { $group=" T2D"; }
-		case /GTRC_VIP/ { $group=" VIP"; }
-		case /GTRC_WEBEX/ { $group=" WEBEX"; }
-		case /GTRC_PORTUGUESE/ { $group=" PORTUGUESE"; }
-		case /GTRC_SPANISH/ { $group=" SPANISH"; }
-		case /GTRC_LWR/ { $group=" LWR"; }
-		case /GTRC_DR_DESKTOP/ { $group=" DR_DESKTOP"; }
-		case /GTRC_MAND_ENG/ { $group=" MANDARIN_ENG"; }
-		case /GTRC_MAND/ { $group=" MANDARIN"; }
-		else	{ $group=$skill; }
-	}
-
-	#initialize $grouped_*{$group} hashes to zero if needed
-	if (!defined($grouped_staffed{$group})) { $grouped_staffed{$group}=0; }
-	if (!defined($grouped_talking{$group})) { $grouped_talking{$group}=0; }
-	if (!defined($grouped_idle{$group})) { $grouped_idle{$group}=0; }
-	if (!defined($grouped_ready{$group})) { $grouped_ready{$group}=0; }
-
-	#add to running total for $group
-	$grouped_staffed{$group} += $staffedskills{$skill}; #staffedskills should never be null
-	if ($talkingskills{$skill}) { $grouped_talking{$group} += $talkingskills{$skill}; }
-	if ($idleskills{$skill}) { $grouped_idle{$group} += $idleskills{$skill}; }
-	if ($readyskills{$skill}) { $grouped_ready{$group} += $readyskills{$skill}; }
-}
-
-#timestamp
-@time = split(/ /, localtime());
-print "$time[3] - $time[0] $time[1] $time[2] $time[4]\n";
-print "\n";
-
-#print out the grouped staffing numbers
-print "                        Staff Avail  Talk  Idle\n";
-print "                        ===== ===== ===== =====\n";
-foreach my $group (sort keys %grouped_staffed) {
-printf ("%-22s %5d %5d %5d %5d\n",$group,$grouped_staffed{$group},$grouped_ready{$group},$grouped_talking{$group},$grouped_idle{$group});
-}
-
-#print holding calls
-print "\n";
-print "Queue            Calls  Time\n";
-print "=====            =====  =====\n";
-foreach my $queue (@{$tree->{queuestatus}->[0]->{queues}}) {
-	printf("%-15s %5s %7s\n",$queue->{queuename},$queue->{queuenumber},$queue->{queuetime});
-}
-
-print "\n"; #trailing blank line
-
 
 #===  FUNCTION  ================================================================
 #         NAME: get_page
-#      PURPOSE: 
 #   PARAMETERS: none
 #      RETURNS: none
 #  DESCRIPTION: retrieves XML from iceberg
@@ -154,3 +48,134 @@ sub get_page {
 	print OUT $mech->content;
 	close(OUT);
 } ## --- end sub get_page
+
+
+#===  FUNCTION  ================================================================
+#         NAME: parse_and_display
+#   PARAMETERS: none
+#      RETURNS: none
+#  DESCRIPTION: Parses the XML from iceberg and displays the information
+#       THROWS: no exceptions
+#     COMMENTS: none
+#     SEE ALSO: n/a
+#===============================================================================
+sub parse_and_display {
+	#Create XML object and pull in the file
+	my $simple = XML::Simple->new();
+	my $tree = $simple->XMLin("$tempfile",ForceArray => 1);
+
+	#undefine variables to reset for each loop
+	undef %staffedskills;
+	undef %talkingskills;
+	undef %idleskills;
+	undef %readyskills;
+	undef %grouped_staffed;
+	undef %grouped_talking;
+	undef %grouped_idle;
+	undef %grouped_ready;
+
+	#get staffing count for talking agents
+	foreach my $analyst (@{$tree->{agentstatus}->[0]->{talking}->[0]->{talkinganalyst}}) {
+		my @skills = split /,/, $analyst->{callskills};
+		foreach my $skill (@skills) {
+			if ($staffedskills{$skill}) {
+				$staffedskills{$skill} += 1;
+			} else {
+				$staffedskills{$skill} = 1;
+			}
+		}
+		if ($talkingskills{$analyst->{talkingon}}) {
+			$talkingskills{$analyst->{talkingon}} += 1;
+		} else {
+			$talkingskills{$analyst->{talkingon}} = 1;
+		}
+	}
+
+	#get staffing count for idle agents
+	foreach my $analyst (@{$tree->{agentstatus}->[0]->{notready}->[0]->{notreadyanalyst}}) {
+		my @skills = split /,/, $analyst->{callskills};
+		foreach my $skill (@skills) {
+			if ($staffedskills{$skill}) {
+				$staffedskills{$skill} += 1;
+			} else {
+				$staffedskills{$skill} = 1;
+			}
+			if ($idleskills{$skill}) {
+				$idleskills{$skill} += 1;
+			} else {
+				$idleskills{$skill} = 1;
+			}
+		}
+	}
+
+	#get staffing count for ready agents
+	foreach my $analyst (@{$tree->{agentstatus}->[0]->{ready}->[0]->{readyanalyst}}) {
+		my @skills = split /,/, $analyst->{callskills};
+		foreach my $skill (@skills) {
+			if ($staffedskills{$skill}) {
+				$staffedskills{$skill} += 1;
+			} else {
+				$staffedskills{$skill} = 1;
+			}
+			if ($readyskills{$skill}) {
+				$readyskills{$skill} += 1;
+			} else {
+				$readyskills{$skill} = 1;
+			}
+		}
+	}
+
+	#combine skills into groups
+	foreach my $skill (sort keys %staffedskills) {
+		#set $group based on $skill
+		switch ($skill) {
+			case /GTRC_DESKTOP/ { $group=" DESKTOP"; }
+			case /GTRC_ENG/ { $group=" ENG"; }
+			case /GTRC_MAIN/ { $group=" MAIN"; }
+			case /GTRC_MOBILITY/ { $group=" MOBILITY"; }
+			case /GTRC_T2D_SPA/ { $group=" T2D_SPANISH"; }
+			case /GTRC_T2D/ { $group=" T2D"; }
+			case /GTRC_VIP/ { $group=" VIP"; }
+			case /GTRC_WEBEX/ { $group=" WEBEX"; }
+			case /GTRC_PORTUGUESE/ { $group=" PORTUGUESE"; }
+			case /GTRC_SPANISH/ { $group=" SPANISH"; }
+			case /GTRC_LWR/ { $group=" LWR"; }
+			case /GTRC_DR_DESKTOP/ { $group=" DR_DESKTOP"; }
+			case /GTRC_MAND_ENG/ { $group=" MANDARIN_ENG"; }
+			case /GTRC_MAND/ { $group=" MANDARIN"; }
+			else	{ $group=$skill; }
+		}
+
+		#initialize $grouped_*{$group} hashes to zero if needed
+		if (!defined($grouped_staffed{$group})) { $grouped_staffed{$group}=0; }
+		if (!defined($grouped_talking{$group})) { $grouped_talking{$group}=0; }
+		if (!defined($grouped_idle{$group})) { $grouped_idle{$group}=0; }
+		if (!defined($grouped_ready{$group})) { $grouped_ready{$group}=0; }
+
+		#add to running total for $group
+		$grouped_staffed{$group} += $staffedskills{$skill}; #staffedskills should never be null
+		if ($talkingskills{$skill}) { $grouped_talking{$group} += $talkingskills{$skill}; }
+		if ($idleskills{$skill}) { $grouped_idle{$group} += $idleskills{$skill}; }
+		if ($readyskills{$skill}) { $grouped_ready{$group} += $readyskills{$skill}; }
+	}
+
+	#timestamp
+	@time = split(/ /, localtime());
+	print "$time[3] - $time[0] $time[1] $time[2] $time[4]\n";
+	print "\n";
+
+	#print out the grouped staffing numbers
+	print "                        Staff Avail  Talk  Idle\n";
+	print "                        ===== ===== ===== =====\n";
+	foreach my $group (sort keys %grouped_staffed) {
+	printf ("%-22s %5d %5d %5d %5d\n",$group,$grouped_staffed{$group},$grouped_ready{$group},$grouped_talking{$group},$grouped_idle{$group});
+	}
+
+	#print holding calls
+	print "\n";
+	print "Queue            Calls  Time\n";
+	print "=====            =====  =====\n";
+	foreach my $queue (@{$tree->{queuestatus}->[0]->{queues}}) {
+		printf("%-15s %5s %7s\n",$queue->{queuename},$queue->{queuenumber},$queue->{queuetime});
+	}
+} ## --- end sub parse_and_display
